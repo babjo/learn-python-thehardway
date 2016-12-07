8ce3a1b7fc456ec213dcdb96f47243a7137293de
babjo <kd980311@naver.com>
2016-12-07 11:11:03 +0900

    Completed Today's learning

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 파이썬 학습 :)
 
+- 실행환경 : Python 2.7.12
+
 참고
 - https://learnpythonthehardway.org/book
---
+++ projects/ex48/tests/parser_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+from ex48 import lexicon
+
+def setup():
+    print "SETUP!"
+
+def teardonw():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
